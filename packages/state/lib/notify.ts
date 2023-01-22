816ac8f14b5a6fb6a3a20e54d603faf12e14c14b
Ibrahim ben Salah <Ibrahim.BenSalah@planonsoftware.com>
2023-01-22 17:15:40 +0100

    git submodules

--- packages/state/lib/notify.ts
+++
@@ -1,9 +0,0 @@
-﻿import { Rx } from './rx';
-
-export function notify<T>(this: Rx.Stateful<T>) {
-  const { observers, snapshot } = this;
-  if (observers !== undefined && snapshot !== undefined)
-    for (const obs of observers as any) {
-      obs.next(snapshot);
-    }
-}
